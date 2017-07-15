cedbb510afd5fa9c95d0ed80566d4e1ed50124f3
z5n0w <836702701zs@gmail.com>
2017-07-15 12:16:10 +0800

    剩下的不想继续搞了23333

--- chnroutes.py
+++ chnroutes.py
@@ -1,8 +1,13 @@
-#!/usr/bin/env python
-
+#encoding: utf-8
 import os
+import math
 import platform
 import requests
+
+def add_one(ori_str):
+    a = int(ori_str)
+    b = a + 1
+    return str(b)
 
 def set_listfile(FILENAME="iplist"):
     my_platform = platform.system()
@@ -24,16 +29,66 @@ def get_iplist(FILENAME=set_listfile()):
         print("iplist exist")
 
 def convert_iplist(LOCATION="CN", LISTFILE=set_listfile()):
-    print()
+    data = open(LISTFILE,"r")
+    regex_str = "%s|ipv4" %(LOCATION)
+    print(regex_str)
+    ipdata = []
+    for line in data:
+        if line.find(regex_str) != -1:
+            ipdata.append(line)
+
+
+    results = []
+    for item in ipdata:
+        unit_items = item.split('|')
+        starting_ip = unit_items[3]
+        num_ip = int(unit_items[4])
+
+        imask = 0xffffffff ^ (num_ip - 1)
+        imask = hex(imask)[2:]
+
+        mask = [imask[i:i + 2] for i in range(0, 8, 2)]
+        mask = '.'.join([str(int(i, 16)) for i in mask])
+
+        cidr = 32 - int(math.log(num_ip, 2))
+
+        results.append((starting_ip, mask, cidr))
+
+    return results
+
+def convert_ipdata(ipdate):
+    rules = open("chnroute.action", "w")
+    for ip, mask, cidr in ipdate:
+        a, b, c ,d = ip.split('.')
+        if cidr > 24:
+            total = 2**(24-cidr)
+            while total > 0:
+                total-=1
+                record = "%s.%s.%s.%s/\n" %(a, b, c, d)
+                d = add_one(d)
+        elif cidr > 16:
+            total = 2**(24-cidr)
+            while total > 0:
+                total-=1
+                record = "%s.%s.%s.*/\n" %(a, b, c)
+                c = add_one(c)
+        elif cidr > 8:
+            total = 2**(16-cidr)
+            while total > 0:
+                total-=1
+                record = "%s.%s.*.*/\n" %(a, b)
+                b = add_one(b)
+        elif cidr > 4:
+            total = 2**(8-cidr)
+            while total > 0:
+                total-=1
+                record = "%s.*.*.*/\n" %(a)
+                a = add_one(a)
+        rules.write(record)
+    rules.close()
 
 def main():
-    data = open(set_listfile())
-    nu = 0
-    for line in data:
-        print(line)
-        nu+=1
-        if nu > 10:
-            break
+    convert_ipdata(convert_iplist())
 
 if __name__ == '__main__':
     main()
